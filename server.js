877403a667026154d82542a5ac7d58c4c3a48b3b
isaacs <i@izs.me>
2011-12-31 17:57:19 -0800

    testing with heroku

--- server.js
+++ server.js
@@ -1,21 +1,17 @@
-(function() {
-  var fs, http, server;
-  http = require('http');
-  fs = require('fs');
-  server = http.createServer(function(req, res) {
-    var s;
-    res.writeHead(200, {
-      "content-type": "text/plain"
-    });
-    s = fs.createReadStream(__filename);
-    s.pipe(res, {
-      end: false
-    });
-    return s.on('end', function() {
-      res.write('\n\n# package.json file:\n');
-      return fs.createReadStream(__dirname + '/package.json').pipe(res);
-    });
-  });
-  server.listen(80);
-  console.log('serving with coffee');
-}).call(this);
+var express = "not found"
+try {
+  express = require.resolve("express")
+} catch (ex) {}
+
+var util = require("util")
+
+require("http").createServer(function (req, res) {
+  var data = { env: process.env
+             , module: module
+             , express: express
+             , pid: process.pid
+             }
+  data = new Buffer(util.inspect(data))
+  res.writeHead(200, { "content-type":"text/plain", "content-length": data.length })
+  res.end(data)
+}).listen(+process.env.PORT || 80)
